666eadf65b56ae9125d178e15ac0a1920b146266
Pius Mutuma <mutumakimathi5@gmail.com>
2024-05-25 09:00:35 +0300

    - Learned joins - FULL, INNER, SELF, RIGHT,OUTER, CROSS

--- Joins/queries.sql
+++ Joins/queries.sql
@@ -1,3 +1,200 @@
-CREATE TABLE presidents (
-    
-)
+-- Exploring cities
+SELECT *
+FROM cities;
+
+-- Exploring countries
+SELECT *
+FROM countries;
+
+-- Joining cities on the left and countries on the right
+SELECT *
+FROM cities
+INNER JOIN countries
+ON cities.country_code = countries.code;
+
+-- Using ALIASES
+SELECT 
+    cities.name AS city, 
+    countries.name AS country, 
+    region
+FROM cities
+INNER JOIN countries
+ON cities.country_code = countries.code;  
+
+-- Joining countries on the left and economies on the right
+SELECT
+    c.code AS country_code,
+    name,
+    year,
+    inflation_rate
+FROM countries as c
+INNER JOIN economies as e
+ON c.code = e.code;
+
+-- Joining with USING
+SELECT
+    c.name AS country,
+    l.name AS language,
+    official
+FROM countries AS c
+INNER JOIN languages AS l
+USING (code);
+
+-- Joining multiple tables
+SELECT
+    c.name AS country,
+    e.year,
+    fertility_rate,
+    unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+INNER JOIN economies AS e
+ON c.code = e.code
+    AND p.year = e.year;
+
+-- LEFT JOIN vs. INNER JOIN
+SELECT
+    c1.name AS city,
+    code,
+    c2.name AS country,
+    region,
+    city_proper_pop
+FROM cities AS c1
+INNER JOIN countries AS c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
+
+SELECT
+    c1.name AS city,
+    code,
+    c2.name AS country,
+    region,
+    city_proper_pop
+FROM cities AS c1
+LEFT JOIN countries AS c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
+
+-- Adding WHERE clause
+SELECT
+    name,
+    region,
+    gdp_percapita
+FROM countries AS c
+LEFT JOIN economies AS e
+ON c.code = e.code
+WHERE year = 2010;
+
+-- RIGHT JOIN
+SELECT 
+    countries.name AS country,
+    languages.name AS language,
+    percent
+FROM languages
+RIGHT JOIN countries
+USING (code);
+
+-- LEFT JOIN alternative
+SELECT 
+    countries.name AS country,
+    languages.name AS language,
+    percent
+FROM countries
+LEFT JOIN languages
+USING (code);
+
+-- FULL JOIN
+-- FULL JOINS combine the results of a LEFT JOIN and a RIGHT JOIN.
+SELECT
+    p1.country AS country,
+    prime_minister,
+    president
+FROM prime_ministers AS p1
+FULL JOIN presidents AS p2
+ON p1.country = p2.country
+LIMIT 10;
+
+-- FULL JOIN (2)
+SELECT 
+    name AS country,
+    code,
+    region,
+    basic_unit
+FROM countries
+FULL JOIN currencies
+USING (code)
+WHERE region = 'North America' OR name IS NULL
+ORDER BY region;
+
+SELECT 
+    name AS country,
+    code,
+    region,
+    basic_unit
+FROM countries
+LEFT JOIN currencies
+USING (code)
+WHERE region = 'North America' 
+    OR name IS NULL
+ORDER BY region;
+
+SELECT 
+    name AS country,
+    code,
+    region,
+    basic_unit
+FROM countries
+INNER JOIN currencies
+USING (code)
+WHERE region = 'North America' OR name IS NULL
+ORDER BY region;
+
+-- Finding languages and currencies
+SELECT
+    c1.name AS country,
+    region,
+    l.name AS language,
+    basic_unit,
+    frac_unit
+FROM countries AS c1
+FULL JOIN languages AS l
+USING (code)
+FULL JOIN currencies AS c2
+USING (code)
+WHERE region LIKE 'M%esia';
+
+-- CROSS JOINS
+-- What are the languages spoken in Pakistan and India?
+-- What languages could have been spoken in the two countries in history?
+SELECT 
+    c.name AS country,
+    l.name AS language
+FROM countries AS c
+INNER JOIN languages AS l
+ON c.code = l.code
+WHERE c.code IN ('IND', 'PAK')
+    AND l.code IN ('IND', 'PAK');
+
+-- Finding names of five countries and their respective regions with the lowest life expectancy in 2010.
+SELECT
+    c.name AS country,
+    region,
+    life_expectancy AS life_exp
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+WHERE year = 2010
+ORDER BY life_exp
+LIMIT 5;
+
+-- SELF JOIN
+SELECT
+    p1.country_code,
+    p1.size AS size2010,
+    p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code
+WHERE p1.year = 2010
+    AND p2.year = 2015;
